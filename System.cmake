dc22d334ea2d6532af52af4a3101de787e3649fe
Jiao Lin <linjiao@caltech.edu>
2015-11-15 08:52:21 -0800

    added FindGSL, System, TestUtils

---
+++ System.cmake
@@ -0,0 +1,11 @@
+# lib64
+get_property(LIB64 GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS)
+if ("${LIB64}" STREQUAL "TRUE")
+    set(LIBSUFFIX 64)
+else()
+    set(LIBSUFFIX "")
+endif()
+set(INSTALL_LIB_DIR
+  lib${LIBSUFFIX} CACHE PATH "Installation directory for libraries")
+# message( STATUS "Install lib dir: ${INSTALL_LIB_DIR}")
+mark_as_advanced(INSTALL_LIB_DIR)
