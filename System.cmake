f70b4d3fe9c50043120275d8c18958590e88dc41
Jiao Lin <linjiao@ornl.gov>
2016-08-02 13:11:18 -0400

    a special case for conda build

--- System.cmake
+++ System.cmake
@@ -5,6 +5,10 @@ if ("${LIB64}" STREQUAL "TRUE")
 else()
     set(LIBSUFFIX "")
 endif()
+# under conda build, always use "lib".
+if ("${CONDA_BUILD}" STREQUAL "TRUE")
+    set(LIBSUFFIX "")
+endif()
 set(INSTALL_LIB_DIR
   lib${LIBSUFFIX} CACHE PATH "Installation directory for libraries")
 # message( STATUS "Install lib dir: ${INSTALL_LIB_DIR}")
